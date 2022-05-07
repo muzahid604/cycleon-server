09b38464b4b12559625dfc4243ecd398f347932f
muzahid604 <604muzahid@gmail.com>
2022-05-07 22:35:15 +0600

    Query added

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ async function run() {
         //delete
         app.delete('/items/:id', async (req, res) => {
             const id = req.params.id;
+            const query = { _id: ObjectId(id) };
         })
     }
     finally {
