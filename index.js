c7d1686f8e8f36aa9046f7a4be30bba1c024ed2d
muzahid604 <604muzahid@gmail.com>
2022-05-08 22:17:13 +0600

    updated the header and footer

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ async function run() {
     try {
         await client.connect();
         const itemsCollection = client.db('cycleWarehouse').collection('items');
-
+        const orderCollection = client.db('cycleWarehouse').collection('order');
         app.get('/items', async (req, res) => {
             const query = {};
             const cursor = itemsCollection.find(query);
@@ -31,6 +31,23 @@ async function run() {
             const item = await itemsCollection.findOne(query);
             res.send(item);
         })
+        //put
+        app.put('/items/:id', async (req, res) => {
+            const id = req.params.id;
+            const updateItems = req.body;
+
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    quantity: updateItems.Quantity
+                }
+            };
+
+            const result = await itemsCollection.updateOne(filter, updatedDoc, options);
+            const answer = await itemsCollection.findOne(filter);
+            res.send(answer);
+        })
 
         //post
         app.post('/items', async (req, res) => {
@@ -38,6 +55,7 @@ async function run() {
             const result = await itemsCollection.insertOne(newItems);
             res.send(result)
         })
+
         //delete
         app.delete('/items/:id', async (req, res) => {
             const id = req.params.id;
@@ -45,6 +63,22 @@ async function run() {
             const result = await itemsCollection.deleteOne(query);
             res.send(result);
         })
+        //order collection api
+        app.post('/order', async (req, res) => {
+            const order = req.body
+            const result = await orderCollection.insertOne(order);
+            res.send(result)
+        })
+        app.get('/order', async (req, res) => {
+            const query = {};
+            const cursor = orderCollection.find(query);
+            const order = await cursor.toArray();
+            res.send(order);
+        });
+
+
+
+
     }
     finally {
 
