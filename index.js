ac8ea6f21d13eb99666477a3472630a633f034a2
muzahid604 <604muzahid@gmail.com>
2022-05-08 06:31:57 +0600

    ready to deploy heroku

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
-
+//deploy to heroku
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pdqpx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
